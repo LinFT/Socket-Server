779aca492443541ac559898505e10321619c8a75
linft <linft1988@gmail.com>
2023-06-07 00:30:21 +0800

    try file_server and file_client

---
+++ file_client.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun  6 23:28:44 2023
+
+@author: linft
+"""
+from socket_handler import SocketHandler
+
+data = 'sssssssssssssssssssss'
+
+client_hdlr = SocketHandler()
+print(client_hdlr)
+
+client_hdlr.connect()
+client_hdlr.send(data)
+
+while True:
+    client_hdlr.recv()
